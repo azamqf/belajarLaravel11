8eee326b1ec341e5b8793ed8df8c37094e70d713
firdaaaaaaa <fnuzula378>
2025-08-01 14:17:28 +0700

    Perbaikan routing dan views

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NetworkController;
 
@@ -9,18 +10,17 @@ Route::get('/radio', function () {
 });
 
 Route::get('/', function () {
-    return view('dashboard'); // pastikan dashboard.blade.php ada
+    return view('dashboard'); // pastikan resources/views/dashboard.blade.php ada
 });
 
 Route::get('/guns', function () {
     return view('guns.index');
-  
+});
+
 Route::get('/admin/radio', function () {
     return view('admin.radio');
-
 });
 
 Route::get('/guns/create', function () {
     return view('guns.create');
 });
-
